3ea319b5c8df533a2f1ba7a0d70718e0b2d4e949
Jan Loennechen <janloennechen@gmail.com>
2023-02-24 16:42:11 +0100

    add image and video func

--- src/components/FileContentContainer.vue
+++ src/components/FileContentContainer.vue
@@ -1,8 +1,6 @@
 <script setup>
 
-import { parse } from "@vue/compiler-dom";
-import { NetCDFReader }  from "netcdfjs";
-import { watchEffect, ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted } from "vue";
   const props = defineProps({
     fileToDisplay: File
   })
@@ -13,27 +11,39 @@ import { watchEffect, ref, onMounted, onUnmounted } from "vue";
 
   const filetype = ref("");
 
+  const imageFileURL = ref(null);
+
   const parser = ref(null);
 
   const reader = new FileReader();
   reader.onload = function(e) {
     const type = props.fileToDisplay.type;
+    console.log(props.fileToDisplay);
     switch (type) {
       case 'image/bmp':
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
         break;
       case 'image/gif':
-
         break;
       case 'image/jpeg':
+    imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
         break;
-      case 'image/png':
+      case 'image/png': 
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
         break;
       case 'image/svg+xml':
         break;
       case 'image/tiff':
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
         break;
       case 'image/webp':
-        break;
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
+        break;  
       case 'text/css':
         break;
       case 'text/xml':
@@ -76,10 +86,28 @@ import { watchEffect, ref, onMounted, onUnmounted } from "vue";
         filecontent.value = parser.value.parseFromString(tempFileContent, "text/xml");
         filetype.value = type;
         break;
+      case 'video/x-matroska':
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
+        console.log(imageFileURL.value);
+        break;
+      case 'video/mp4':
+        imageFileURL.value = generateURL(props.fileToDisplay);
+        filetype.value = type;
+        console.log(imageFileURL.value);
+        break;
     }
  
   }
-  reader.readAsText(props.fileToDisplay);
+  props.fileToDisplay.type.split("/")[0] === 'image' ? reader.readAsDataURL(props.fileToDisplay) : reader.readAsText(props.fileToDisplay);
+
+  function generateURL(file){
+    let fileSrc = URL.createObjectURL(file);
+    setTimeout(() => {
+      URL.revokeObjectURL(fileSrc);
+    }, 1000)
+    return fileSrc;
+  }
 
   function deleteItem(e){
     emit('file-deleted', props.fileToDisplay);
@@ -88,8 +116,10 @@ import { watchEffect, ref, onMounted, onUnmounted } from "vue";
   // prevent default prowser behaviour
 function preventDefaults(e) {
     e.preventDefault()
-}
+  }
 
+const events = ['click']
+  
 onMounted(() => {
     events.forEach((eventName) => {
         document.body.addEventListener(eventName, preventDefaults)
@@ -102,23 +132,31 @@ onUnmounted(() => {
     })
 })
 
-const events = ['click']
-
 </script>
 
 <template>
   
   <div class="fileview">
-    <h2>{{fileToDisplay.name}} <button class="x-button" @click="deleteItem">X</button> </h2>  
-                                                     
+    <h2>{{fileToDisplay.name}} <button class="x-button" @click="deleteItem">X</button> </h2>                                                     
     <div  id="file_content_zone" class="container">
-   
-    <div class="text-in-container">
-      <p v-if="filetype === 'application/vnd.ms-excel' || filetype === 'text/csv'" v-for="(l,i) in filecontent">{{ i }} : {{ l }} </p>
-<pre :class="{ code : filetype === 'application/json' }" v-else>
+      <div v-if="filetype === 'application/vnd.ms-excel' || filetype === 'text/csv'" class="text-in-container">
+        <p  v-for="(l,i) in filecontent" :key="i">{{ i }} : {{ l }} </p>
+      </div>
+      <div v-else-if="filetype === 'image/png' || filetype === 'image/jpeg'" class="image-container">
+        <img :src="generateURL(fileToDisplay)"/>
+      </div>
+      <div v-else-if="filetype === 'video/x-matroska' || filetype === 'video/mp4'" class="image-container">
+        <video width="320" height="240" controls class="image-container">
+        <source :src="generateURL(fileToDisplay)" type="video/mp4" controls>
+        <source :src="generateURL(fileToDisplay)" type="video/webm" controls>
+        Your browser does not support the video tag.
+      </video> 
+      </div>
+      <div v-else class="text-in-container">
+<pre :class="{ code : filetype === 'application/json' }">
 {{ filecontent }}
 </pre>
-  </div>
+      </div>
   </div>
   </div>
   
@@ -133,7 +171,8 @@ const events = ['click']
         padding: 2em 2em;
         font-size: 1.5em;
         display: flex;
-        flex: 0 0 0 100%;overflow: break-word;
+        flex: 0 0 0 100%;
+        overflow: break-word;
       }
 
     .text-in-container {
@@ -154,7 +193,8 @@ const events = ['click']
     }
 
     .code{
-      color: rgb(245, 86, 86);
+      /* color: rgb(245, 86, 86); */
+      color: var(--color-text);
     }
 
     .x-button {
@@ -163,5 +203,16 @@ const events = ['click']
       border-radius: 6px;
       text-align: center;
       font-weight: bold;
+    }
+
+    .image-container{
+      display: flex;
+      width: 100%;
+      height: 100%;
+      justify-content: center;
+    }
+    
+    img{
+      width: 75%;
     }
 </style>
